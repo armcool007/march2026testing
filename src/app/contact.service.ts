c5d5c56bfe12010b9d9f110487dbe3cc1b240411
armcool007 <bipinkk004@gmail.com>
2026-05-17 01:48:10 +0530

    Update contact.service.ts

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact';
 })
 export class ContactService {
 
-  private baseUrl="http://15.206.171.79:8080/"
+  private baseUrl="http://15.206.171.79:8081/"
 
   constructor(private httpClient:HttpClient) { }
 
